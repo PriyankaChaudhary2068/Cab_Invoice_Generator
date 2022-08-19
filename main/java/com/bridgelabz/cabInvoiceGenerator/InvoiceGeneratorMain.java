023fe8bf17d99969e42e3a57330708ca7621b2e8
chutki12 <priyankachaudhary2068>
2022-08-19 23:29:01 +0530

    ADDED : Welcome To Cab Invoice Generator Program

---
+++ main/java/com/bridgelabz/cabInvoiceGenerator/InvoiceGeneratorMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.cabInvoiceGenerator;
+
+public class InvoiceGeneratorMain {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Welcome To Cab Invoice Generator Program");
+
+	}
+
+}
